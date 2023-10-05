3f508a43b4a17b4e5c3cc96eb84c153dc8156def
김태훈 <nuckly60@gmail.com>
2023-10-05 17:05:09 +0900

    초기세팅

---
+++ src/MainGameManagement/MainGameManagement.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './MainGameManagement.css';
+
+function MainGameManagement() {
+  return (
+    <div className="MainGameManagement">
+    </div>
+  );
+}
+
+export default MainGameManagement;
